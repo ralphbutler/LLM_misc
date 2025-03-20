338873fb3d8781dbc9c80aab1f38d55d5c13a768
ralphbutler <143019451+ralphbutler@users.noreply.github.com>
2025-03-20 13:34:47 -0500

    demo of building MCP servers with all: tools, resources, prompts
    demo of building MCP servers with all: tools, resources, prompts.
    We pretend to be developing code for dealing with ARC challenge puzzles, and develop endpoints for each of tools, resources, and, prompts.  Only tools are usable by ClaudeDesktop and ClaudeCode.

---
+++ MCP_ARC_server/temp.sh
@@ -0,0 +1,6 @@
+
+claude mcp add arc_server -s user -- \
+    uv --directory /Users/rbutler/MCPSERVERS/arc_server \
+    run arc_server.py \
+
+# claude mcp remove -s user arc_server  ### to remove it later
